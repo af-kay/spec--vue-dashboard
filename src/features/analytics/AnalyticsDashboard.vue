7c4bcf316e2fb7539cc5ec7f08a6462a79a77ceb
A. F. Kay <afkay.dev@yandex.ru>
2023-03-02 03:58:28 +0300

    Add analytics menu & content section previews

--- src/features/analytics/AnalyticsDashboard.vue
+++ src/features/analytics/AnalyticsDashboard.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import SideMenu from './ui/SideMenu.vue';
+import DashboardContent from './ui/DashboardContent.vue';
 </script>
 
 <template>
   <div class="AnalyticsDashboard">
     <SideMenu />
+    <DashboardContent />
   </div>
 </template>
 
 <style scoped lang="scss">
-  .AnalyticsDashboard {
-    border: 1px dashed pink;
-    background: #e2f0f0;
-    height: 100%;
-    display: flex;
-  }
+.AnalyticsDashboard {
+  border: 1px dashed pink;
+  background: #e2f0f0;
+  height: 100%;
+  display: flex;
+}
 </style>
---
+++ src/features/analytics/ui/DashboardContent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="DashboardContent">
+    <div class="TitleRow">
+      <h4>Dashboard</h4>
+      <div class="Interactions">
+        <div class="DateRange">
+          <input type="text" disabled placeholder="DD/MM/YYYY" />
+        </div>
+        <button>print</button>
+        <button>download</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '@fontsource/quicksand';
+@import '@/shared/styles';
+
+* {
+  font-family: 'Quicksand', sans-serif;
+}
+
+.DashboardContent {
+  border: 1px dashed maroon;
+  flex: 1;
+  padding: 12px;
+
+  .TitleRow {
+    display: flex;
+    justify-content: space-between;
+
+    .Interactions {
+      display: flex;
+      column-gap: 4px;
+    }
+  }
+}
+</style>
